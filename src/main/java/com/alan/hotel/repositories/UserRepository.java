df0b1b52efd65c0197341876d14e4a627a595ea1
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2018-05-14 10:15:03 +0200

    Created Account controller and User entity

---
+++ src/main/java/com/alan/hotel/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.alan.hotel.repositories;
+
+import com.alan.hotel.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(@Param("username") String username);
+}
